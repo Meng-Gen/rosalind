50235da2ee4a93fdb4956505d8e5d1a6ba1a8741
Meng-Gen <plover@gmail.com>
2013-12-12 23:23:11 +0800

    Bio.SeqIO

--- BioinformaticsArmory/TFSQ/tfsq.py
+++ BioinformaticsArmory/TFSQ/tfsq.py
@@ -4,7 +4,7 @@ def main():
     from Bio import SeqIO
     fastq_dict = SeqIO.index("in.txt", "fastq")
     for fastq in fastq_dict:
-        print(fastq_dict[fastq].format("fasta"))    
+        print(fastq_dict[fastq].format("fasta"))
     
 if __name__ == '__main__':
     sys.exit(main())
