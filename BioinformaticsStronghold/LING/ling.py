30782b561f5f58e87f928d487bd2fe3eb0a88c63
Meng-Gen <plover@gmail.com>
2013-12-13 22:20:46 +0800

    Hard and tricky, bucket sort

--- BioinformaticsStronghold/LING/ling.py
+++ BioinformaticsStronghold/LING/ling.py
@@ -3,14 +3,24 @@ import sys
 
 def get_substring_count(string):
     n = len(string)
-    suffix_list = []
-    for i in range(n):
-        suffix_list.append(string[i:])
-    suffix_list.sort()
     substring_count = n * (n+1) // 2
-    for i in range(1, n):
-        substring_count -= get_longest_common_prefix(suffix_list[i-1], suffix_list[i])
+    for alphabet_bucket in 'ACGT':
+        count = get_substring_count_helper(string, alphabet_bucket)
+        substring_count -= count
     return substring_count
+
+# Avoid out of memory by bucket sort    
+def get_substring_count_helper(string, alphabet_bucket):
+    suffix_list = []
+    for i in range(len(string)):
+        suffix = string[i:]
+        if suffix[0] == alphabet_bucket:
+            suffix_list.append(suffix)
+    suffix_list.sort()
+    count = 0
+    for i in range(1, len(suffix_list)):
+        count += get_longest_common_prefix(suffix_list[i-1], suffix_list[i])
+    return count
     
 def get_maximum_substring_count(string):
     n = len(string)
@@ -20,19 +30,15 @@ def get_maximum_substring_count(string):
     
 def get_linguistic_complexity(string):
     substring_count = get_substring_count(string)
-    #print(substring_count)
     maximum_substring_count = get_maximum_substring_count(string)
-    #print(maximum_substring_count)
     return float(substring_count) / float(maximum_substring_count)
         
 def get_longest_common_prefix(x, y):
     n = min(len(x), len(y))
-    longest_common_pos = n
     for i in range(n):
         if x[i] != y[i]:
-            longest_common_pos = i
-            break
-    return longest_common_pos
+            return i
+    return n
     
 def main():
     dna_string = sys.stdin.readline().strip()
