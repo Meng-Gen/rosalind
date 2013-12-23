2b48c2237cdf4f7ad6ef0ec85a162ff3a490b409
Meng-Gen <plover@gmail.com>
2013-12-23 21:44:03 +0800

    Tricky

--- TextbookTrack/1G/1g.py
+++ TextbookTrack/1G/1g.py
@@ -1,58 +1,58 @@
 import itertools
 import sys
 
-def all_possible_matching_kmers(genome, k, d):
-    all_possible_kmers = set()
-    for i in range(len(genome)-k+1):
-        motif = genome[i:i+k]
-        kmers = approximating_matching_kmers(motif, k, d)
-        all_possible_kmers |= kmers
-        print(kmers)
-    return all_possible_kmers
+class ApproximateMatchingProblem():
+    def __init__(self, genome, k, d):
+        self.genome = genome
+        self.k = k
+        self.d = d
+        self.approximating_motifs = None
     
-def approximating_matching_kmers(motif, k, d):
-    all_kmers = set()
-    all_mismatch_patterns = set()
-    for mismatch_pattern in itertools.permutations(['D']*d + ['M']*(k-d), k):
-        all_mismatch_patterns.add(''.join(mismatch_pattern))
-    for pattern in all_mismatch_patterns:
-        for nucleotides in itertools.product('ACGT', repeat=d):
-            kmer = ''
-            mismatch_count = 0
-            for i in range(k):
-                if pattern[i] == 'M':
-                    kmer += motif[i]
+    def get_most_frequent_motifs(self):
+        self.approximating_motifs = {}
+        m = len(self.genome) - self.k + 1
+        for i in range(m):
+            piece = self.genome[i:i+self.k]
+            self.__update_approximating_motifs(piece)
+        return self.__get_most_frequent_motifs_impl()
+    
+    def __update_approximating_motifs(self, piece):
+        for pattern in itertools.product([False, True], repeat=self.k):
+            count = pattern.count(False)
+            if count > self.d:
+                continue
+            approximating_patterns = []
+            for i in range(self.k):
+                if not pattern[i]:
+                    possible_nucleotides = list('ACGT')
+                    possible_nucleotides.remove(piece[i])
+                    approximating_patterns.append(possible_nucleotides)
                 else:
-                    kmer += nucleotides[mismatch_count]
-                    mismatch_count += 1
-            all_kmers.add(kmer)
-    return all_kmers
-
-def approximating_matching_count(genome, kmer, d):
-    count = 0
-    m, n = len(kmer), len(genome)
-    for i in range(n):
-        motif = genome[i:i+m]
-        if len(motif) == m and is_approximating_matching(motif, kmer, d):
-            count += 1
-    return count
+                    approximating_patterns.append([piece[i]])
+            for x in itertools.product(*approximating_patterns):
+                motif = ''.join(x)
+                if motif not in self.approximating_motifs:
+                    self.approximating_motifs[motif] = 0
+                self.approximating_motifs[motif] += 1
     
-def is_approximating_matching(p, q, d):
-    return sum([1 if p[i] != q[i] else 0 for i in range(len(p))]) <= d
-    
+    def __get_most_frequent_motifs_impl(self):
+        max_freq_so_far = 0
+        max_freq_motifs_so_far = set()
+        for motif in self.approximating_motifs:
+            freq = self.approximating_motifs[motif]
+            if freq > max_freq_so_far:
+                max_freq_so_far = freq
+                max_freq_motifs_so_far = set()
+                max_freq_motifs_so_far.add(motif)
+            elif freq == max_freq_so_far:
+                max_freq_motifs_so_far.add(motif)
+        return max_freq_motifs_so_far
+        
 def main():
     genome = sys.stdin.readline().strip()
     k, d = map(int, sys.stdin.readline().strip().split())
+    problem = ApproximateMatchingProblem(genome, k, d)
+    print(' '.join(list(problem.get_most_frequent_motifs())))
     
-    max_count, max_count_kmers = 0, []
-    for kmer in all_possible_matching_kmers(genome, k, d):
-        count = approximating_matching_count(genome, kmer, d)
-        print(kmer, count)
-        if count == max_count:
-            max_count_kmers.append(kmer)
-        elif count > max_count:
-            max_count, max_count_kmers = count, [kmer]
-    print(' '.join(max_count_kmers))
-
 if __name__ == '__main__':
     sys.exit(main())
