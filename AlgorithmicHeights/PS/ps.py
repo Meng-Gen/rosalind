457f4aaa94a4748885807a6525e4389fa7994cbc
Meng-Gen <plover@gmail.com>
2013-12-23 18:34:04 +0800

    Sorting hack again

---
+++ AlgorithmicHeights/PS/ps.py
@@ -0,0 +1,15 @@
+import sys
+
+def read_dataset():
+    lines = [line.strip() for line in sys.stdin.readlines()]
+    n, k = int(lines[0]), int(lines[2])
+    unsorted_array = list(map(int, lines[1].split()))
+    return n, unsorted_array, k
+
+def main():
+    n, unsorted_array, k = read_dataset()
+    unsorted_array.sort()
+    print(' '.join(map(str, unsorted_array[:k])))
+    
+if __name__ == '__main__':
+    sys.exit(main())
