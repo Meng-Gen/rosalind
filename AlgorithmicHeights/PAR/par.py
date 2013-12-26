7c06246402cefecea6332ddcfa655e9586f99c24
Meng-Gen <plover@gmail.com>
2013-12-26 13:16:58 +0800

    Hoare partition

--- AlgorithmicHeights/PAR/par.py
+++ AlgorithmicHeights/PAR/par.py
@@ -22,11 +22,24 @@ class QuickSort():
                 self.array[i], self.array[j] = self.array[j], self.array[i]
         self.array[i+1], self.array[self.size] = self.array[self.size], self.array[i+1]
         return self.array[1:]        
+
+    def hoare_partition(self):
+        x = self.array[1]
+        i, j = 1, self.size
+        while True:
+            while self.array[j] > x:
+                j -= 1
+            while self.array[i] < x:
+                i += 1
+            if i < j:
+                self.array[i], self.array[j] = self.array[j], self.array[i]
+            else:
+                return self.array[1:]
         
 def main():
     n, array = read_dataset()
     quick_sort = QuickSort(n, array)
-    print(' '.join(map(str, quick_sort.partition())))
+    print(' '.join(map(str, quick_sort.hoare_partition())))
     
 if __name__ == '__main__':
     sys.exit(main())
